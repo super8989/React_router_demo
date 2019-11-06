185aede70edb1a968f3ee740b00ef44069a8a399
Sam <super8989@gmail.com>
2019-11-06 18:53:44 -0500

    added starter styles

--- src/Dog.js
+++ src/Dog.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Dog extends Component {
 	render() {
 		return (
-			<div className='dog'>
+			<div className='Dog'>
 				<h1>Dog</h1>
 				<img src='https://images.unsplash.com/photo-1529927066849-79b791a69825?ixlib=rb-1.2.1&auto=format&fit=crop&w=1649&q=80' />
 			</div>
