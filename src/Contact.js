4fb18c91e7424f1176087a0c9886d0613e1baa79
Sam <super8989@gmail.com>
2019-11-06 18:47:12 -0500

    boilerplate for react router demo

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+	render() {
+		return (
+			<div className='Contact'>
+				<h1>Contact!</h1>
+				<p>No thank you!</p>
+			</div>
+		);
+	}
+}
+
+export default Contact;
---
+++ src/Dog.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Dog extends Component {
+	render() {
+		return (
+			<div className='dog'>
+				<h1>Dog</h1>
+				<img src='https://images.unsplash.com/photo-1529927066849-79b791a69825?ixlib=rb-1.2.1&auto=format&fit=crop&w=1649&q=80' />
+			</div>
+		);
+	}
+}
+
+export default Dog;
